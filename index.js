3a3de18b95244371aedb5251664304d31e51b052
Amy Cen <amy.cen@blackrock.com>
2019-06-24 19:31:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function superbowlWin(records) {
   let found_record = records.find(function(record){return record.result==='W'})
-  found_record? return found_record.year : return found_record
+  
 }
